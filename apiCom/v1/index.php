c756f43abadf929d46a3412fc617867ad869b3ae
Juan <juan.paez@api-ux.com>
2023-11-15 15:11:44 -0500

    pr base de prueba

--- apiCom/v1/index.php
+++ apiCom/v1/index.php
@@ -319,6 +319,104 @@ Flight::route('POST /postStore/@apk/@xapk', function ($apk,$xapk) {
 
 
 
+Flight::route('POST /postCategorie/@apk/@xapk', function ($apk,$xapk) {
+  
+    header("Access-Control-Allow-Origin: *");
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apk) && !empty($xapk)) {    
+        // Leer los datos de la solicitud
+       
+
+
+
+
+        
+
+
+
+
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->domKairos();
+        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKey/';
+      
+        $data = array(
+            'apiKey' =>$apk, 
+            'xApiKey' => $xapk
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response11 = curl_exec($curl);
+
+      
+
+
+      curl_close($curl);
+
+      
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response11 == 'true' ) {
+
+
+
+            $clientId= Flight::request()->data->clientId;
+            $categoryName= Flight::request()->data->categoryName;
+            $comments= Flight::request()->data->comments;
+            $parentId= Flight::request()->data->parentId;
+            $categoryType= Flight::request()->data->categoryType;
+
+
+            require_once '../../apiCom/v1/model/modelSecurity/uuid/uuidd.php';
+           
+   
+
+            $gen_uuid = new generateUuid();
+            $myuuid = $gen_uuid->guidv4();
+         
+
+            $categoryId = substr($myuuid, 0, 8);
+
+         
+            $conectar=conn();
+
+           
+            $query = mysqli_query($conectar, "INSERT INTO generalCategories (catId, clientId, catName, comments, parentId) VALUES ('$categoryId', '$clientId', '$categoryName', '$comments', '$parentId','$categoryType')");
+
+            if ($query) {
+                echo "true|¡Categoria creada con éxito!";
+            } else {
+                // Si hay un error, imprime el mensaje de error
+                echo "false|" . mysqli_error($conectar);
+            }
+            
+           
+     
+
+       
+        
+           // echo json_encode($response1);
+        } else {
+            echo 'false|¡Autenticación fallida!';
+           // echo json_encode($data);
+        }
+    } else {
+        echo 'false|¡Encabezados faltantes!';
+    }
+});
+
+
+
 
 
 Flight::route('GET /getCalendarTime/@filter/@param', function ($filter,$param) {
