d3d18a9719fd09abcad50591e20d54451efa1df8
Juan <juan.paez@api-ux.com>
2023-11-16 13:03:59 -0500

    pr base de prueba

--- apiCom/v1/index.php
+++ apiCom/v1/index.php
@@ -733,7 +733,7 @@ if($filter=="all"){
 
           
            
-    $query= mysqli_query($conectar,"SELECT storeId,storeName,comments,isActive,storeType FROM generalStores where ca.clientId='$clientId'");
+    $query= mysqli_query($conectar,"SELECT storeId,storeName,comments,isActive,storeType FROM generalStores where clientId='$clientId'");
 
 
 }
@@ -741,7 +741,7 @@ if($filter=="all"){
 if($filter!="all"){
 
           
-    $query= mysqli_query($conectar,"SELECT storeId,clientId,storeName,comments,isActive,storeType FROM generalStores where ca.clientId='$clientId' and $param='$value'");
+    $query= mysqli_query($conectar,"SELECT storeId,clientId,storeName,comments,isActive,storeType FROM generalStores where clientId='$clientId' and $param='$value'");
 
 }
 
