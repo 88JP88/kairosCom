29b3f393b86c1555fddf25da79f319becb100b1a
Juan <juan.paez@api-ux.com>
2023-11-16 12:38:44 -0500

    pr base de prueba

--- apiCom/v1/index.php
+++ apiCom/v1/index.php
@@ -600,7 +600,7 @@ if($filter=="allone"){
 
           
            
-    $query= mysqli_query($conectar,"SELECT ca.catalogId,ca.clientId,ca.productId,ca.categoryId,ca.stock,ca.secStock,ca.minQty,ca.maxQty,ca.storeId,ca.outPrice,ca.promoId,ca.isActive,ca.discount,ca.isPromo,ca.isDiscount,ca.isEcommerce,ca.isPos,ca.isInternal,ca.isStocked,ca.unit,ca.readUnit,ca.unitQty,ca.unitUnit FROM generalCatalogs ca ");
+    $query= mysqli_query($conectar,"SELECT ca.catalogId,ca.clientId,ca.productId,ca.categoryId,ca.stock,ca.secStock,ca.minQty,ca.maxQty,ca.storeId,ca.outPrice,ca.promoId,ca.isActive,ca.discount,ca.isPromo,ca.isDiscount,ca.isEcommerce,ca.isPos,ca.isInternal,ca.isStocked,ca.unit,ca.readUnit,ca.unitQty,ca.unitUnit FROM generalCatalogs ca");
 
 
 }
@@ -658,7 +658,7 @@ if ($query) {
                         $value=[
                             'productId' => $row['productId'],
                             'clientId' => $row['clientId'],
-                            'productName' => $row['productName'],
+                            
                             'catalogId' => $row['catalogId'],
                             'categoryId' => $row['categoryId'],
                             'stock' => $row['stock'],
