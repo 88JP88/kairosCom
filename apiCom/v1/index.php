dbdcfbb36e11cb3a8e40a27fc698aec2ebb5449c
Juan <juan.paez@api-ux.com>
2023-11-16 13:02:58 -0500

    pr base de prueba

--- apiCom/v1/index.php
+++ apiCom/v1/index.php
@@ -674,6 +674,120 @@ if ($query) {
 
 
 
+Flight::route('GET /getStores/@clientId/@filter/@param/@value', function ($clientId,$filter,$param,$value) {
+    header("Access-Control-Allow-Origin: *");
+    // Leer los encabezados
+    $headers = getallheaders();
+    
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (isset($headers['Api-Key']) ) {
+        // Leer los datos de la solicitud
+       
+        // Acceder a los encabezados
+        $apiKey = $headers['Api-Key'];
+        $xApiKey = $headers['x-api-Key'];
+        
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->domKairos();
+        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyKairos/';
+      
+        $data = array(
+          'apiKey' =>$apiKey, 
+          'xApiKey' => $xApiKey
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response1 = curl_exec($curl);
+
+      
+
+
+      curl_close($curl);
+
+      
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response1 == 'true' ) {
+           
+
+
+
+           
+            $conectar=conn();
+            
+        
+         
+     
+if($filter=="all"){
+
+          
+           
+    $query= mysqli_query($conectar,"SELECT storeId,storeName,comments,isActive,storeType FROM generalStores where ca.clientId='$clientId'");
+
+
+}
+
+if($filter!="all"){
+
+          
+    $query= mysqli_query($conectar,"SELECT storeId,clientId,storeName,comments,isActive,storeType FROM generalStores where ca.clientId='$clientId' and $param='$value'");
+
+}
+
+
+
+if ($query) {
+
+                $values=[];
+          
+                while($row = $query->fetch_assoc())
+                {
+                   
+                        $value=[
+                            'storeId' => $row['storeId'],
+                            'storeName' => $row['storeName'],
+                            'comments' => $row['comments'],
+                            'isActive' => $row['isActive'],
+                            'storeType' => $row['storeType'],
+                            'clientId' => $row['clientId']
+                        ];
+                        
+                        array_push($values,$value);
+                        
+                }
+                $row=$query->fetch_assoc();
+                //echo json_encode($students) ;
+                echo json_encode(['stores'=>$values]);
+          
+               
+                
+                } else {
+                    // Si hay un error, imprime el mensaje de error
+                    echo "false|" . mysqli_error($conectar);
+                }
+
+        } else {
+            echo 'Error: Autenticación fallida';
+             //echo json_encode($response1);
+        }
+    } else {
+        echo 'Error: Encabezados faltantes';
+    }
+});
+
+
+
 
 Flight::route('GET /getClientRooms/@filter/@timeid', function ($filter,$timeid) {
     header("Access-Control-Allow-Origin: *");
