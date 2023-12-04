480b59fbf0cd4afa6cf73a004fa1665be7a98942
Juan <juan.paez@api-ux.com>
2023-12-03 19:36:15 -0500

    pr base de prueba

--- apiClient/v1/index.php
+++ apiClient/v1/index.php
@@ -1647,14 +1647,14 @@ $_SESSION['nProducts']=$_SESSION['nProducts']+$productQty;
         }
     } else {
         if ($query) {
-
+$ar=json_encode($cart);
           $fTotal=  $_SESSION['fTotal'];
             $fsTotal=$_SESSION['fsTotal'];
             $fSaver=$_SESSION['fSaver'];
             $npro=$_SESSION['nProducts'];
             $npa=$_SESSION['nPacks'];
           //  $productName = $arrayData[0]['payment']['total'];
-          $query1 = mysqli_query($conectar, "INSERT INTO generalOrders (orderId,carId, clientId, userId, shopperId, storeType, storeId, totalAmount, subtotalAmount, orderProgress, saver, fromIp, fromStore, fromBrowser, orderPayload, paymentMethod, returnCash, transactionStatus,numberProducts,numberPacks) VALUES ('$orderId','$cartId','$clientId','$userId','$userId','POS','$storeId',$fTotal,$fsTotal,'PENDING',$fSaver,'$fromIp','$storeId','$fromBrowser',$cart,'CASH',0,'PENDING',$npro,$npa)");
+          $query1 = mysqli_query($conectar, "INSERT INTO generalOrders (orderId,carId, clientId, userId, shopperId, storeType, storeId, totalAmount, subtotalAmount, orderProgress, saver, fromIp, fromStore, fromBrowser, orderPayload, paymentMethod, returnCash, transactionStatus,numberProducts,numberPacks) VALUES ('$orderId','$cartId','$clientId','$userId','$userId','POS','$storeId',$fTotal,$fsTotal,'PENDING',$fSaver,'$fromIp','$storeId','$fromBrowser',$ar,'CASH',0,'PENDING',$npro,$npa)");
       if($query1){
         echo "true|¡Orden creada con éxito!";
       } else {
