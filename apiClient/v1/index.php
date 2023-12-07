7453a435a78814c69385e5a943f94fb834d1bf08
Juan <juan.paez@api-ux.com>
2023-12-07 15:53:44 -0500

    pr base de prueba

--- apiClient/v1/index.php
+++ apiClient/v1/index.php
@@ -2904,4 +2904,113 @@ Flight::route('POST /postCustomer/@apk/@xapk', function ($apk,$xapk) {
 });
 
 
+Flight::route('GET /getCustomers/@clientId/@filter/@param/@value', function ($clientId,$filter,$param,$value) {
+    header("Access-Control-Allow-Origin: *");
+    // Leer los encabezados
+    $headers = getallheaders();
+    
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (isset($headers['Api-Key']) ) {
+        // Leer los datos de la solicitud
+       
+        // Acceder a los encabezados
+        $apiKey = $headers['Api-Key'];
+        $xApiKey = $headers['x-api-Key'];
+        
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->domKairos();
+        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyKairos/';
+      
+        $data = array(
+          'apiKey' =>$apiKey, 
+          'xApiKey' => $xApiKey
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response1 = curl_exec($curl);
+
+      
+
+
+      curl_close($curl);
+
+      
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response1 == 'true' ) {
+           
+
+
+
+           
+            $conectar=conn();
+            
+          if($filter=="all"){
+
+          
+           
+                $query= mysqli_query($conectar,"SELECT customerId,clientId,customerName,customerLastName,customerMail,customerPoints,customerPhone,customerStars,customerType,isActive FROM generalCustomers where clientId='$clientId'");
+        }
+         
+      
+if($filter=="filter"){
+
+          
+           
+    $query= mysqli_query($conectar,"SELECT customerId,clientId,customerName,customerLastName,customerMail,customerPoints,customerPhone,customerStars,customerType,isActive FROM generalCustomers where clientId='$clientId' and $param='$value'");
+
+
+}
+
+
+
+                $values=[];
+          
+                while($row = $query->fetch_assoc())
+                {
+                   
+                        $value=[
+                            'customerId' => $row['customerId'],
+                            'clientId' => $row['clientId'],
+                            'customerName' => $row['customerName'],
+                            'customerLastName' => $row['customerLastName'],
+                            'customerMail' => $row['customerMail'],
+                            'customerPoints' => $row['customerPoints'],
+                            'customerPhone' => $row['customerPhone'],
+                            
+                            'customerStars' => $row['customerStars'],
+                            'customerType' => $row['customerType'],
+                            'isActive' => $row['isActive']
+                        ];
+                        
+                        array_push($values,$value);
+                        
+                }
+                $row=$query->fetch_assoc();
+                //echo json_encode($students) ;
+                echo json_encode(['customers'=>$values]);
+          
+               
+           
+
+        } else {
+            echo 'Error: Autenticación fallida';
+             //echo json_encode($response1);
+        }
+    } else {
+        echo 'Error: Encabezados faltantes';
+    }
+});
+
 Flight::start();
