704f5d1c366e7fd2c7cda59d950e39c7b23da5b2
Juan <juan.paez@api-ux.com>
2023-12-14 23:14:15 -0500

    pr base de prueba

--- apiClient/v1/index.php
+++ apiClient/v1/index.php
@@ -3519,4 +3519,123 @@ if($filter=="byStore"){
 
 
 
+
+
+
+Flight::route('POST /sendEcmValCode/@apk/@xapk', function ($apk,$xapk) {
+        
+    header("Access-Control-Allow-Origin: *");
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apk) && !empty($xapk)) {    
+        // Leer los datos de la solicitud
+    
+
+
+
+
+        
+
+
+
+
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->domKairos();
+        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKey/';
+    
+        $data = array(
+            'apiKey' =>$apk, 
+            'xApiKey' => $xapk
+        
+        );
+    $curl = curl_init();
+    
+    // Configurar las opciones de la sesión cURL
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    
+    // Ejecutar la solicitud y obtener la respuesta
+    $response11 = curl_exec($curl);
+
+    
+
+
+    curl_close($curl);
+
+    
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response11 == 'true' ) {
+
+
+
+            $clientId= Flight::request()->data->clientId;
+            $customerMail= Flight::request()->data->customerMail;
+            require_once '../../apiClient/v1/model/modelSecurity/uuid/uuidd.php';
+                
+
+            $gen_uuid = new generateUuid();
+            $myuuid = $gen_uuid->guidv4();
+        
+
+            $valCode = substr($myuuid, 0, 8);
+
+        
+        
+            $conectar=conn();
+            $query3 = mysqli_query($conectar, "SELECT COUNT(customerId) as cusId from generalCustomers WHERE customerMail='$customerMail' AND clientId='$clientId'");
+            
+            // Verificar si la consulta fue exitosa
+            
+                // Obtener la primera fila como un arreglo asociativo
+                $fila = $query3->fetch_assoc();
+            
+                // Verificar si la fila tiene datos
+                if ($fila) {
+                    // Obtener el valor de la columna 'coId'
+                    //mensaje al correo del clientr
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+$from = "confirmation@lugma.tech";
+$to = $customerMail;
+$subject = "Código de confirmación para compra";
+
+$message = 'Tu Código de confirmación es:  '.$valCode;
+
+
+$headers = "From:" . $from;
+mail($to,$subject,$message, $headers);
+                    $query = mysqli_query($conectar, "UPDATE generalCustomers SET ecmCode='$valCode' where clientId='$clientId' and customerId IN (SELECT customerId WHERE customerMail='$customerMail' and clientId='$clientId')");
+                    if ($query) {
+                        echo "true|¡Código enviado con éxito al mail ".$customerMail." !";
+                    } else {
+                        // Si hay un error, imprime el mensaje de error
+                        echo "false|" . mysqli_error($conectar);
+                    }
+                }else{
+
+                    echo "false|¡Debes agregarte como cliente!";
+                }
+            
+        
+          
+    
+        
+        // echo json_encode($response1);
+        } else {
+            echo 'false|¡Autenticación fallida!';
+        // echo json_encode($data);
+        }
+    } else {
+        echo 'false|¡Encabezados faltantes!';
+    }
+});
+
+
+
+
 Flight::start();
