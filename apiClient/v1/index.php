579348349c9e8d9817a0ed753aad1a52a2cb590b
Juan <juan.paez@api-ux.com>
2023-12-19 11:25:15 -0500

    pr base de prueba

--- apiClient/v1/index.php
+++ apiClient/v1/index.php
@@ -3765,4 +3765,121 @@ mail($to,$subject,$message, $headers);
 
 
 
+
+Flight::route('GET /getClientPickupPoints/@clientId/@filter/@param/@value', function ($clientId,$filter,$param,$value) {
+    header("Access-Control-Allow-Origin: *");
+    // Leer los encabezados
+    $headers = getallheaders();
+    
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (isset($headers['Api-Key']) ) {
+        // Leer los datos de la solicitud
+    
+        // Acceder a los encabezados
+        $apiKey = $headers['Api-Key'];
+        $xApiKey = $headers['x-api-Key'];
+        
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->domKairos();
+        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyKairos/';
+    
+        $data = array(
+        'apiKey' =>$apiKey, 
+        'xApiKey' => $xApiKey
+        
+        );
+    $curl = curl_init();
+    
+    // Configurar las opciones de la sesión cURL
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    
+    // Ejecutar la solicitud y obtener la respuesta
+    $response1 = curl_exec($curl);
+
+    
+
+
+    curl_close($curl);
+
+    
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response1 == 'true' ) {
+        
+
+
+
+        
+            $conectar=conn();
+            
+        if($filter=="all"){
+
+        
+        
+            $query= mysqli_query($conectar,"SELECT pickupId,clientId,pointName,pointAdd,isActive,cityPoint,suscribedTo FROM pickupPoints where clientId='$clientId'");
+        }
+        
+    
+if($filter=="byParam"){
+
+        
+        
+    $query= mysqli_query($conectar,"SELECT pickupId,clientId,pointName,pointAdd,isActive,cityPoint,subscribedTo FROM pickupPoints where clientId='$clientId' and $param='$value'");
+       
+
+}
+
+
+
+
+
+                $values=[];
+        
+                while($row = $query->fetch_assoc())
+                {
+                
+                        $value=[
+                            'clientId' => $row['clientId'],
+                            'pickupId' => $row['pickupId'],
+                            'pointName' => $row['pointName'],
+                            'pointAdd' => $row['pointAdd'],
+                            'isActive' => $row['isActive'],
+                            'cityPoint' => $row['cityPoint'],
+                            'subscribedTo' => $row['subscribedTo']
+                            
+                        ];
+                        
+                        array_push($values,$value);
+                        
+                }
+                $row=$query->fetch_assoc();
+                //echo json_encode($students) ;
+                echo json_encode(['pickupPoints'=>$values]);
+        
+                if ($query) {
+                   
+                   // echo "true|¡Producto actualizado con éxito!";
+                } else {
+                    // Si hay un error, imprime el mensaje de error
+                    echo "false|" . mysqli_error($conectar);
+                }
+        
+
+        } else {
+            echo 'Error: Autenticación fallida';
+            //echo json_encode($response1);
+        }
+    } else {
+        echo 'Error: Encabezados faltantes';
+    }
+});
+
+
+
 Flight::start();
