9fc09f2142e5bccfbdf9abce79701c7bf3cec1b3
Juan <juan.paez@api-ux.com>
2024-01-18 09:55:14 -0500

    pr base de prueba

--- apiCom/v1/index.php
+++ apiCom/v1/index.php
@@ -7,6 +7,7 @@ require_once 'model/users/responses.php';
 require 'model/modelSecurity/authModule.php';
 require_once 'env/domain.php';
 require_once 'kronos/postLog.php';
+require_once 'model/users/getModel.php';
 
 
 
