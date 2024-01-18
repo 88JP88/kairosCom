0d5ca910196d745d13ac48707cc643d1ee2ad2d4
Juan <juan.paez@api-ux.com>
2024-01-18 12:57:16 -0500

    pr base de prueba

--- apiCom/v1/index.php
+++ apiCom/v1/index.php
@@ -463,8 +463,8 @@ Flight::route('GET /getCategories/@clientId/@filter/@param/@value', function ($c
                 'value' => $value
             );
 
-//echo modelGet::getCategories($dta);
-  echo json_encode($dta);         
+echo modelGet::getCategories($dta);
+//  echo json_encode($dta);         
 
 }else { 
     
