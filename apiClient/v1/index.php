83d86f8d912fa1cfe817e9ab35dcaa9ecd17ffe1
Juan <juan.paez@api-ux.com>
2024-01-25 11:50:33 -0500

    pr base de prueba

--- apiClient/v1/index.php
+++ apiClient/v1/index.php
@@ -952,7 +952,7 @@ Flight::route('GET /getClientOrders/@apiData', function ($apiData) {
            
            
 //echo modelGet::getOrders($postData);
-     echo $postData;      
+     echo $decodedData;      
 
 }else { 
     
